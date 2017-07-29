508d6a5e54d14726b9dd2843767e9ea0b25dad59
Keaton <keaton.wallace@gmail.com>
2017-07-29 15:51:52 -0700

    Added file dialog. Single line of text displayed on richtextbox

--- RobotLogViewer/HomePage.xaml.cs
+++ RobotLogViewer/HomePage.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+//using Microsoft.Win32;
+
 
 namespace RobotLogViewer
 {
@@ -23,6 +27,29 @@ namespace RobotLogViewer
         public HomePage()
         {
             InitializeComponent();
+        }
+        private void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            
+            // Process input if the user clicked OK.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it the textbox.
+                    richTextBox1.AppendText(reader.ReadLine());
+                }
+                fileStream.Close();
+            }
         }
     }
 }
