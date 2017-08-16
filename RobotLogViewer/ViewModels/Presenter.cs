e3adc810fcdc3d3b05b5bee0003c16e781644b79
Keaton <keaton.wallace@gmail.com>
2017-08-15 18:34:29 -0700

    Broke the button

--- RobotLogViewer/ViewModels/Presenter.cs
+++ RobotLogViewer/ViewModels/Presenter.cs
@@ -3,10 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections.ObjectModel;
 
 namespace RobotLogViewer.ViewModels
 {
     class Presenter
     {
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        private void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            // Process input if the user clicked OK.
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it the textbox.
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        _history.Add(line);
+                        //richTextBox1.AppendText(line);
+                        //richTextBox1.AppendText(System.Environment.NewLine);
+                    }
+
+                }
+                fileStream.Close();
+            }
+        }
     }
 }
--- RobotLogViewer/Views/HomePage.xaml.cs
+++ RobotLogViewer/Views/HomePage.xaml.cs
@@ -29,34 +29,6 @@ namespace RobotLogViewer.Views
         {
             InitializeComponent(); 
         }
-        private void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
-        {
-            // Create an instance of the open file dialog box.
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
-
-            // Set filter options and filter index.
-            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
-            
-            // Process input if the user clicked OK.
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                // Open the selected file to read.
-                System.IO.Stream fileStream = openFileDialog1.OpenFile();
-
-                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
-                {
-                    // Read the first line from the file and write it the textbox.
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        richTextBox1.AppendText(line);
-                        richTextBox1.AppendText(System.Environment.NewLine);
-                    }
-                    
-                }
-                fileStream.Close();
-            }
-        }
+        
     }
 }
