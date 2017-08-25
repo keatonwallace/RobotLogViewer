81ab08a476d43b84711464c237f3e4ce87b0290b
Keaton <keaton.wallace@gmail.com>
2017-08-24 20:07:29 -0700

    Little debugging

--- RobotLogViewer/ViewModels/Presenter.cs
+++ RobotLogViewer/ViewModels/Presenter.cs
@@ -27,36 +27,38 @@ namespace RobotLogViewer.ViewModels
             get { return _history; }
         }
 
-        //I don't have the focus tonight to do much. I will do better tomorrow
-        private void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
+        public void bOpenFileDialog_Click(object sender, RoutedEventArgs e)
         {
-            // Create an instance of the open file dialog box.
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //I don't think the field is being redrawn once the information is there
+            _history.Add("something");
+            //// Create an instance of the open file dialog box.
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //Console.WriteLine("I am here");
 
-            // Set filter options and filter index.
-            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
-            openFileDialog1.FilterIndex = 1;
+            //// Set filter options and filter index.
+            //openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            //openFileDialog1.FilterIndex = 1;
 
-            // Process input if the user clicked OK.
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            {
-                // Open the selected file to read.
-                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+            //// Process input if the user clicked OK.
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    // Open the selected file to read.
+            //    System.IO.Stream fileStream = openFileDialog1.OpenFile();
 
-                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
-                {
-                    // Read the first line from the file and write it the textbox.
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        _history.Add(line);
-                        //richTextBox1.AppendText(line);
-                        //richTextBox1.AppendText(System.Environment.NewLine);
-                    }
+            //    using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+            //    {
+            //        // Read the first line from the file and write it the textbox.
+            //        string line;
+            //        while ((line = reader.ReadLine()) != null)
+            //        {
+            //            _history.Add(line);
+            //            //richTextBox1.AppendText(line);
+            //            //richTextBox1.AppendText(System.Environment.NewLine);
+            //        }
 
-                }
-                fileStream.Close();
-            }
+            //    }
+            //    fileStream.Close();
+            //}
         }
     }
 }
